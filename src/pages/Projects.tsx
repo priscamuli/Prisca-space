a92b06f5307ac62f606f8f4bec55c101a2088446
priscamuli <priscakamanthe31@gmail.com>
2026-03-16 12:21:29 +0300

    added a new project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,8 @@ import ProjectCard from "../components/ProjectCard";
 function Projects(){
     const projectList = [
         {title: "Ecommerce Site", description:"Javascrip + Django app", link:"https://josmayawebsite.onrender.com/"},
-        {title: "Portfolio", description:"React-based personal site", link:"https://prisca-space.vercel.app/"}
+        {title: "Portfolio", description:"React-based personal site", link:"https://prisca-space.vercel.app/"},
+        {title: "MedAccess", description:"Health Medical website", link:"https://medaccess-bridge.vercel.app/"}
     ];
     return(
         <section className="min-h-screen min-w-screen bg-black text-white py-16 px-8">
